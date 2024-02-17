e954f9152be052432b54e334fb6faafbf1be0516
ljq0226 <1035821043@qq.com>
2024-02-17 09:51:11 +0800

    add:add new one

--- code/5.数组转对象.js
+++ code/5.数组转对象.js
@@ -6,6 +6,25 @@ const arr = [
   { node: 'f', children: ['g'] },
   { node: 'e', children: ['h'] },
 ]
+const array2Tree = (arr)=>{
+  const res = {}
+  const temp = {}
+  const childrenArr = []
+  for(const item of arr){
+    const {node,children} = item
+    childrenArr.push(...children)
+    temp[node] = {}
+    children.forEach(child=>temp[node][child] = null)
+  }
+  for(const key in temp){
+    const isRoot = !childrenArr.includes(key)
+    if(isRoot) res[key] = temp[key]
+    for(const i in temp[key]){
+      temp[key][i] = temp[i] || null
+    }
+  }
+  return res
+}
 //==>
 // {
 //   d: {
@@ -25,45 +44,25 @@ const arr = [
 //   }
 // }
 
-function array2Tree(array){
+console.log(JSON.stringify(array2Tree(arr), null, 2))
+
+// 给你一个对象{'a':1,'b.c.d':2,'b.c.e':3}，实现一个函数把它展开成{'a':1,'b':{'c':{'d':2,'e':3}}}这种形式。
+const obj = { a: 1, 'b.c.d': 2, 'b.c.e': 3 }
+function flattenObject(obj) {
   const res = {}
-  const childArray = []
-  const temp = {}
-  array.forEach(i=>{
-    childArray.push(...i.children)
-    temp[i.node] = {}
-    i.children.forEach(item=>temp[i.node][item]=null)
-  })
-  for (const key in temp) {
-    const isRoot = !childArray.includes(key)
-    if(isRoot) res[key] = temp[key]
-    for(const i in temp[key]){
-      temp[key][i] = temp[i] || null
+  for (const key in obj) {
+    const value = obj[key]
+    const parts = key.split('.')
+    let current = res
+    if (parts.length == 1) {
+      current[parts[0]] = value
+    } else {
+      for (const part of parts.slice(0, -1)) {
+        current = current[part] = current[part] || {}
+      }
+      current[parts.pop()] = value
     }
   }
   return res
-
 }
-
-// function array2Tree(array) {
-//   const res = {}
-//   const temp = {}
-//   const childList = []
-//   array.forEach((item) => {
-//     const { node, children } = item
-//     childList.push(...children)
-//     temp[node] = {}
-//     children.forEach((child) => (temp[node][child] = null))
-//   })
-//   for (const key in temp) {
-//     const isRoot = !childList.includes(key)
-//     isRoot && (res[key] = temp[key])
-//     for (const j in temp[key]) {
-//       temp[key][j] = temp[j] || null
-//     }
-//   }
-//   return temp
-// }
-
-
-console.log(JSON.stringify(array2Tree(arr), null, 2))
+console.log(JSON.stringify(flattenObject(obj), null, 2))
